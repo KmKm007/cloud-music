6f0e8f34b03f11ad1048316a4c03a54e49af1a87
KmKm <kmkmhuang007@gmail.com>
2017-07-03 18:01:38 +0800

    import

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -88,10 +88,11 @@ module.exports = {
     // for React Native Web.
     extensions: ['.web.js', '.js', '.json', '.web.jsx', '.jsx'],
     alias: {
-      
+
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
+      '@styles': paths.styles
     },
     plugins: [
       // Prevents users from importing files from outside of src/ (or node_modules/).
@@ -118,12 +119,13 @@ module.exports = {
           {
             options: {
               formatter: eslintFormatter,
-              
+
             },
             loader: require.resolve('eslint-loader'),
           },
         ],
         include: paths.appSrc,
+        exclude: [ /registerServiceWorker.js/ ]
       },
       // ** ADDING/UPDATING LOADERS **
       // The "file" loader handles all assets unless explicitly excluded.
@@ -167,7 +169,7 @@ module.exports = {
         include: paths.appSrc,
         loader: require.resolve('babel-loader'),
         options: {
-          
+
           // This is a feature of `babel-loader` for webpack (not Babel itself).
           // It enables caching results in ./node_modules/.cache/babel-loader/
           // directory for faster rebuilds.
